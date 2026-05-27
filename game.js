7dbc1ad7c1c1b2b242b3773451193dd3adbfd170
papairoland <papai.roli12@gmail.com>
2026-05-27 10:10:00 +0200

    feat(js): játékos alapértelmezett statisztikáinak és állapotváltozóinak definiálása

---
+++ game.js
@@ -0,0 +1,11 @@
+let player = {
+  skill:      10,
+  stamina:    20,
+  maxStamina: 20,
+  luck:       8,
+  maxLuck:    8,
+  food:       2,
+  items:      [],
+};
+let combat = null;
+let currentPassageId = 1;
